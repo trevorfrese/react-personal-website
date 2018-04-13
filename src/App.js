b6368002557b36f7f29d3afc88697d828bad0a42
Trevor Frese <trevor.frese@gmail.com>
2018-04-12 23:29:44 -0700

    add in routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 // import Particles from './components/Particles/Particles';
-// import Storyline from './components/Storyline/Storyline';
+import Storyline from './components/Storyline/Storyline';
 import HomePage from './components/HomePage/HomePage';
+import RoleModels from './components/RoleModels/RoleModels';
+
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 // import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import Contact from './components/Contact/Contact';
@@ -10,10 +13,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage/>
+        <Switch>
+          {/* <Route exact path="/work" component={Work} /> */}
+          <Route exact path="/my_story" component={Storyline} />
+          <Route exact path="/role_models" component={RoleModels} />
+          <Route path="/" component={HomePage} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/RoleModels/RoleModels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './RoleModels.css';
+
+const roleModels = (props) => (
+  <div className={classes.RoleModels}>
+    Insert role models here
+  </div>
+);
+
+export default roleModels;
