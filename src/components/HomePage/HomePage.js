5ed85afbc07f0439014f369bfe5bbd1347f29b8c
Trevor Frese <trevor.frese@gmail.com>
2020-01-19 19:42:20 -0800

    Adds Fireworks and Welcome pages, created using anime.js
    Rebranding the website for 2020, with a new brighter color palette and some use of neon

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,38 +1,47 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import Fireworks from '../Fireworks/Fireworks';
+import Background from '../UI/Text/Background/Background';
+import Welcome from '../Welcome/Welcome';
 
-import mainImage from '../../assets/images/just-face.jpg';
+
+import mainImage from '../../assets/images/waterfall.jpg';
 
 import classes from './HomePage.css';
-
+// <div className={classes.CanvasOverlay}>
+//   <div className={classes.Row}>
+//     <div className={classes.ImageContainer}>
+//       <img className={classes.MainImage} src={mainImage} alt="" />
+//         <p className={classes.Greeting}>
+//           <Background backgroundColor="white" fontColor="#000">
+//             I'm Trevor Frese
+//           </Background>
+//         </p>
+//     </div>
+//   </div>
+//   <div className={classes.Controls}>
+//     <div className={classes.Control + ' ' + classes.ButtonContainer}>
+//       <Link to="/work">
+//         <button className={classes.Button + ' ' + classes.Blue}>WORK</button>
+//       </Link>
+//     </div>
+//     <div className={classes.Control + ' ' +  classes.ButtonContainer}>
+//       <Link to="/my_story">
+//         <button className={classes.Button + ' ' + classes.Purple}>WHO I AM</button>
+//       </Link>
+//     </div>
+//     <div className={classes.Control + ' ' + classes.ButtonContainer}>
+//       <Link to="/mentors">
+//         <button className={classes.Button + ' ' + classes.Orange}>MENTORS</button>
+//       </Link>
+//     </div>
+//   </div>
+// </div>
 const homePage = (props) => (
   <div className={classes.HomePage}>
-    <div className={classes.Row}>
-      <div className={classes.ImageContainer}>
-        <img className={classes.MainImage} src={mainImage} alt="" />
-        <p className={classes.Greeting}>I'm Trevor Frese</p>
-      </div>
-    </div>
-    <div className={classes.Controls}>
-      <div className={classes.Control + ' ' + classes.ButtonContainer}>
-        <Link to="/work">
-          <button className={classes.Button + ' ' + classes.Blue}>WORK</button>
-        </Link>
-      </div>
-      <div className={classes.Control + ' ' +  classes.ButtonContainer}>
-        <Link to="/my_story">
-          <button className={classes.Button + ' ' + classes.Purple}>WHO I AM</button>
-        </Link>
-      </div>
-      <div className={classes.Control + ' ' + classes.ButtonContainer}>
-        <Link to="/mentors">
-          <button className={classes.Button + ' ' + classes.Orange}>MENTORS</button>
-        </Link>
-      </div>
-    </div>
-
-
+    <Welcome/>
+    <Fireworks/>
   </div>
 );
 
